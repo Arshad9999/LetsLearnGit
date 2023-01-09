b231e924d6faebb727622ecacf4708bd3bec4930
Mohammad Arshad Siddiqui <93732499+Arshad9999@users.noreply.github.com>
2023-01-09 23:35:53 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Exercise6 {
+public class sum{
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Input first number: ");
