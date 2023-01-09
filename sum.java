714098e6e468313dec2d8fdfc7ca3b3baeb4b0d3
Mohammad Arshad Siddiqui <93732499+Arshad9999@users.noreply.github.com>
2023-01-09 23:35:17 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -1,6 +1,17 @@
-public class sum{
+import java.util.Scanner;
+public class Exercise6 {
     public static void main(String[] args) {
-        int a=5, b=6;
-        System.out.println("SUM = "+a+b);
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input first number: ");
+        int num1 = in.nextInt();
+        
+        System.out.print("Input second number: ");
+        int num2 = in.nextInt();
+        
+        System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+        System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+        System.out.println(num1 + " x " + num2 + " = " + (num1 * num2));
+        System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
+        System.out.println(num1 + " mod " + num2 + " = " + (num1 % num2));
     }
 }
